7a44d714a11e1ac86bb328e9a65fb97066d8697f
Khalifa rmili@MacOS <khalifa.rmili@gmail.com>
2023-01-23 13:51:31 +0100

    cleanups

--- app/Http/Livewire/JobButton.php
+++ app/Http/Livewire/JobButton.php
@@ -17,12 +17,6 @@ class JobButton extends Component
 
     public function runJob()
     {
-        // Log::info("Job button clicked");
-        // for ($x = 1; $x <= 2; $x++) {
-        //     CalculateDataJob::dispatch($x);
-        // }
-        // $downloadFileJob = new DownloadCSVFileJob('myData.csv', 'https://raw.githubusercontent.com/amitkaps/multidim/master/data/pincode.csv');
-
         Log::info("dispatching downloadFileJob");
         // $downloadFileJob::dispatch();
         // DownloadCSVFileJob::dispatch('myData.csv', 'https://raw.githubusercontent.com/amitkaps/multidim/master/data/pincode.csv');
